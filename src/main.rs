0623d129b57d41a1b70604c90d2d58929ba3320c
amandio <amandio@lsd.ufcg.edu.br>
2023-04-26 01:23:19 +0000

    Add certificate lib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod certificate;
+
 use axum::{routing::{post, get}, Json, Router, http::StatusCode, response::IntoResponse,};
 use axum::extract::Path;
 use axum::extract::ConnectInfo;
@@ -5,6 +7,14 @@ use axum_macros::debug_handler;
 use std::net::SocketAddr;
 use tokio::time::{sleep, Duration};
 use std::env;
+
+use serde::{Deserialize, Serialize};
+use crate::certificate::certificate::gen_cert;
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Certificate {
+    cert: String,
+    key: String,
+}
 
 #[tokio::main]
 async fn main() {
@@ -28,6 +38,22 @@ async fn start_main_server() {
         .serve(app.into_make_service_with_connect_info::<SocketAddr>())
         .await
         .unwrap();
+}
+
+async fn generate_certificate() -> impl IntoResponse {
+    let (cert, rsa) = gen_cert().unwrap();
+
+    // Convert certificate and key to PEM format
+    let cert_pem = cert.to_pem().unwrap();
+    let key_pem = rsa.private_key_to_pem_pkcs8().unwrap();
+
+    // Convert to base64-encoded JSON
+    let cert_json = Certificate {
+        cert: base64::encode(cert_pem),
+        key: base64::encode(key_pem),
+    };
+
+    serde_json::to_string(&cert_json).unwrap()
 }
 
 async fn create_cert( Json(config): Json< String >) -> impl IntoResponse {
